57699ef2a3e76d74864041b5e5a988adf9bc7f5f
Cal <cal@caldotcom.com>
2024-12-29 21:09:00 +0000

    Changed layer sort, included metadata standard for OpenSea, prompt edits to allow clearer naming of metadata through layers to create rarity naming

--- generate.js
+++ generate.js
@@ -1,122 +1,127 @@
-// Simple art generator by HashLips <->
+// Simple art generator by HashLips <-> https://github.com/HashLips/hashlips_art_engine_ps_script
+// Contributed to by caldotcom <-> https://github.com/caldotcom
 
 #include "./lib/json2.js";
 
 function main() {
-  var continueConfirmation = confirm(
-    "You are about to use the HashLips art generator. Are you sure you want to continue?"
-  );
+	var continueConfirmation = confirm(
+		"You are about to use the HashLips art generator. Are you sure you want to continue?"
+	);
 
-  if (!continueConfirmation) return;
+	if (!continueConfirmation) return;
 
-  var supply = prompt("How many images do you want to generate?", "10");
+	var supply = prompt("How many images do you want to generate?", "10");
+	var name = prompt("What is the name of your collection?", "");
+	var description = prompt("What is the description for your collection?", "");
 
-  var name = prompt("What is the name of your collection?", "");
+	alert(
+		supply +
+		" images will be generated, so sit back relax and enjoy the art being generated."
+	);
 
-  var description = prompt("What is the description for your collection?", "");
+	var groups = app.activeDocument.layerSets;
+	resetLayers(groups);
 
-  alert(
-    supply +
-      " images will be generated, so sit back relax and enjoy the art being generated."
-  );
+	for (var h = 1; h <= parseInt(supply); h++) {
+		var obj = {};
+		obj.name = name + " #" + h;
+		obj.description = description;
+		obj.image = "build/images/" + h + ".jpg";
+		obj.edition = h;
+		obj.attributes = [];
 
-  var groups = app.activeDocument.layerSets;
+		for (var i = 0; i < groups.length; i++) {
+			var totalWeight = 0;
+			var layerMap = [];
 
-  resetLayers(groups);
+			for (var j = 0; j < groups[i].layers.length; j++) {
+				totalWeight += getRWeights(groups[i].layers[j].name);
+				layerMap.push({
+					index: j,
+					name: cleanName(groups[i].layers[j].name),
+					weight: getRWeights(groups[i].layers[j].name)
+				});
+			}
 
-  function getRWeights(_str) {
-    var weight = Number(_str.split("#").pop());
-    if(isNaN(weight)){
-      weight = 1;
-    }
-    return weight;
-  }
+			var ran = Math.floor(Math.random() * totalWeight);
 
-  function cleanName(_str) {
-    return _str.split("#").shift();
-  }
+			(function() {
+				for (var j = 0; j < groups[i].layers.length; j++) {
+					ran -= layerMap[j].weight;
+					if (ran < 0) {
+						groups[i].layers[j].visible = true;
+						obj.attributes.push({
+							trait_type: groups[i].name,
+							value: layerMap[j].name.replace(/ /g, "\\ ")
+						});
+						return;
+					}
+				}
+			})();
+		}
+		saveImage(obj.edition, obj.attributes);
+		saveMetadata(obj);
+		resetLayers(groups);
+	}
 
-  for (var h = 1; h < parseInt(supply) + 1; h++) {
-    var obj = {};
-    obj.name = name + " #" + h;
-    obj.description = description;
-    obj.image = "To be replaced";
-    obj.edition = h;
-    obj.attributes = [];
-    for (var i = 0; i < groups.length; i++) {
-      var totalWeight = 0;
-      var layerMap = [];
-
-      for(var j = 0; j < groups[i].layers.length; j++){
-        totalWeight += getRWeights(groups[i].layers[j].name);
-        layerMap.push({
-          index: j,
-          name: cleanName(groups[i].layers[j].name),
-          weight: getRWeights(groups[i].layers[j].name)
-        });
-      }
-
-      var ran = Math.floor(Math.random() * totalWeight);
-
-      (function() {
-        for(var j = 0; j < groups[i].layers.length; j++){
-          ran -= layerMap[j].weight;
-          if(ran < 0) {
-            groups[i].layers[j].visible = true;
-            obj.attributes.push({
-              trait_type: groups[i].name, 
-              value: layerMap[j].name
-            })
-            return;
-          }
-        }
-      })();
-    }
-    saveImage(obj.edition);
-    saveMetadata(obj);
-    resetLayers(groups);
-  }
-  alert("Generation process is complete.");
+	alert("Generation process is complete.");
 }
 
-function resetLayers(_groups) {
-  for (var i = 0; i < _groups.length; i++) {
-    _groups[i].visible = true;
-    for (var j = 0; j < _groups[i].layers.length; j++) {
-      _groups[i].layers[j].visible = false;
-    }
-  }
+function resetLayers(layerGroups) {
+	for (var i = 0; i < layerGroups.length; i++) {
+		layerGroups[i].visible = true;
+		for (var j = 0; j < layerGroups[i].layers.length; j++) {
+			layerGroups[i].layers[j].visible = false;
+		}
+	}
 }
 
-function saveImage(_edition) {
-  var saveFile = new File(toFolder("build/images") + "/" + _edition + ".png");
-  exportOptions = new ExportOptionsSaveForWeb();
-  exportOptions.format = SaveDocumentType.PNG;
-  exportOptions.PNG24 = false;
-  exportOptions.transparency = true;
-  exportOptions.interlaced = false;
-  app.activeDocument.exportDocument(
-    saveFile,
-    ExportType.SAVEFORWEB,
-    exportOptions
-  );
+function getRWeights(str) {
+	var weight = Number(str.split("#").pop());
+	if (isNaN(weight)) {
+		weight = 1;
+	}
+	return weight;
 }
 
-function saveMetadata(_data) {
-  var file = new File(toFolder("build/metadata") + "/" + _data.edition + ".json");
-  file.open("w");
-  file.write(JSON.stringify(_data));
-  file.close();
+function cleanName(str) {
+	return str.split("#").shift();
 }
 
-function toFolder(_name) {
-  var path = app.activeDocument.path;
-  var folder = new Folder(path + "/" + _name);
-  if (!folder.exists) {
-    folder.create();
-  }
-  return folder;
+function saveImage(edition, attributes) {
+	var saveFile = new File(toFolder("build/images") + "/" + edition + ".jpg");
+	var exportOptions = new ExportOptionsSaveForWeb();
+	exportOptions.format = SaveDocumentType.JPEG;
+	exportOptions.quality = 100; // Highest quality
+	app.activeDocument.info.title = "Edition " + edition;
+	app.activeDocument.info.caption = JSON.stringify(attributes);
+	app.activeDocument.exportDocument(
+		saveFile,
+		ExportType.SAVEFORWEB,
+		exportOptions
+	);
 }
 
+function saveMetadata(data) {
+	var metadata = {
+		name: data.name,
+		description: data.description,
+		image: data.image,
+		edition: data.edition,
+		attributes: data.attributes
+	};
+
+	var file = new File(toFolder("build/metadata") + "/" + data.edition + ".json");
+	file.open("w");
+	file.write(JSON.stringify(metadata, null, 2));
+	file.close();
+}
+
+function toFolder(folderName) {
+	var path = app.activeDocument.path;
+	var folder = new Folder(path + "/" + folderName);
+	if (!folder.exists) folder.create();
+	return folder;
+}
 
 main();
--- update_metadata.js
+++ update_metadata.js
@@ -1,39 +1,41 @@
-// Simple art generator by HashLips <->
+// Simple art generator by HashLips <-> https://github.com/HashLips/hashlips_art_engine_ps_script
+// Contributed to by caldotcom <-> https://github.com/caldotcom
 
 #include "./lib/json2.js";
 
-function main () {
+function main() {
+	var continueConfirmation = confirm(
+		"You are about to update the metadata of your collection. Are you sure you want to continue?"
+	);
 
-    var continueConfirmation = confirm(
-        "You are about to update the metadata of your collection. Are you sure you want to continue?"
-      );
-      
-      if(!continueConfirmation) return
-      
-      var prefix = prompt("What is your new metadata prefix?", "");
-      
-      var suffix = prompt("What is you new metadata suffix?", "");
+	if (!continueConfirmation) return;
 
-      var path = app.activeDocument.path;
-      var f = new Folder(path + "/build/metadata");
-      
-      if (f != null) {
-        var fileList = f.getFiles(/\.(json)$/i);
-      }
-      
-      for (var a = 0; a < fileList.length; a++) {
-        var file = File(fileList[a]);
-        file.open("r");
-        var data = JSON.parse(file.read());
-        file.close();
-        data.image = prefix + data.edition + suffix;
-        file.open("w");
-        file.write(JSON.stringify(data));
-        file.close();
-      }
+	var prefix = prompt("What is your new metadata prefix?", "");
+	var suffix = prompt("What is your new metadata suffix?", "");
 
-      alert("Updating metadata process is complete.");
-      
+	var path = app.activeDocument.path;
+	var metadataFolder = new Folder(path + "/build/metadata");
+
+	if (metadataFolder.exists) {
+		var fileList = metadataFolder.getFiles(/\.(json)$/i);
+
+		for (var i = 0; i < fileList.length; i++) {
+			var file = new File(fileList[i]);
+			file.open("r");
+			var data = JSON.parse(file.read());
+			file.close();
+
+			data.image = prefix + data.edition + suffix;
+
+			file.open("w");
+			file.write(JSON.stringify(data, null, 2));
+			file.close();
+		}
+
+		alert("Updating metadata process is complete.");
+	} else {
+		alert("Metadata folder does not exist.");
+	}
 }
 
-main()
+main();
